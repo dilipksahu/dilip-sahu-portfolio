2f52e96786a66f990f8fbcffabcf325eb773e56a
Dilip Sahu <dilip.sahu@targetpeak.in>
2025-03-03 21:48:15 +0530

    loading sreen add margin and home text size change  for mobile layout

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -21,7 +21,7 @@ function LoadingScreen({onComplete}) {
 
   return (
     <div className='fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center'>
-      <div className='mb-4 text-4xl font-mono font-bold'>
+      <div className='mb-4 text-2xl md:text-4xl font-mono font-bold mx-14 md:mx-20'>
         {text} <span className='animate-blink ml-1'> ! </span>
       </div>
       <div className='w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden'>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <section id="home" className="min-h-screen flex items-center justify-center relative">
       <RevealOnScroll>
-        <div className="text-center z-10 px-4">
+        <div className="text-center z-10 px-4 mt-2">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
             {data.home.title}
           </h1>
