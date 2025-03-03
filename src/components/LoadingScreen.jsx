eaaa36aa7196c5c6b689221f8e710c19a3aefdfb
Dilip Sahu <dilip.sahu@targetpeak.in>
2025-03-03 22:05:34 +0530

    margin and padding add in loading and home resp

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -20,8 +20,8 @@ function LoadingScreen({onComplete}) {
   },[onComplete]);
 
   return (
-    <div className='fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center ml-10 md:ml-20 mr-20'>
-      <div className='mb-4 text-2xl md:text-4xl font-mono font-bold'>
+    <div className='fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center '>
+      <div className='mb-4 text-2xl md:text-4xl font-mono font-bold ml-20 md:ml-20 mr-35 md:mr-0'>
         {text} <span className='animate-blink ml-1'> ! </span>
       </div>
       <div className='w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden'>
